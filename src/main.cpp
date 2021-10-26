3bf82d5cbb64f8f4ee856178b73cd2aed48a2001
LysyProgramista <golem5693@gmail.com>
2021-10-26 19:04:11 +0200

    fg

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
-#include <raylib.h>
+#include "raylib.h"
+#include <iostream>
+
+using namespace std;
+
+int ball(int x, int y, int width, int height)
+{
+    DrawRectangle(x, y, width, height, BLUE);
+}
 
 int main(void)
 {
@@ -13,27 +21,27 @@ int main(void)
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-
+    
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        
         // Update
         //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 3.0f;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 3.0f;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= 3.0f;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 3.0f;
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
+            ball(0, 0, 10, screenHeight);
             ClearBackground(RAYWHITE);
-
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
-
-            DrawCircleV(ballPosition, 50, MAROON);
+                
+            DrawRectangle(ballPosition.x, ballPosition.y, 60, 30, BLUE);
+            
 
         EndDrawing();
         //----------------------------------------------------------------------------------
